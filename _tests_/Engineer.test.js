77ac0f3f3933250bedfe67c27aa6b18cf7fb2c2f
Achu Ajesh <achuv37@gmail.com>
2022-04-15 09:56:09 -0400

    Modified engineer test file

---
+++ _tests_/Engineer.test.js
@@ -0,0 +1,34 @@
+const Engineer = require("../lib/Engineer.js");
+
+describe("Engineer", () => {
+  const name = "Mathew";
+  const id = 10;
+  const email = "Mathew@gmail.com";
+  const role = "Engineer";
+  const githubUsername = "Mathew01";
+  const githubLink = `<a href ="https://github.com/${githubUsername}">https://github.com/${githubUsername}</a>`;
+
+  const engineer = new Engineer(name, id, email, githubUsername);
+
+  it("should return a engineer name when it called", () => {
+    expect(engineer.getName()).toBe(name);
+  });
+  
+  it("should return a engineer Id when it called", () => {
+    expect(engineer.getId()).toBe(id);
+  });
+
+  it("should return a engineer email when it called", () => {
+    expect(engineer.getEmail()).toBe(email);
+  });
+
+  it("should return a engineer github link when it called", () => {
+    expect(engineer.getGithub()).toBe(githubLink);
+  });
+
+  it("should return a engineer role when it called", () => {
+    expect(engineer.getRole()).toBe(role);
+  });
+
+});
+
